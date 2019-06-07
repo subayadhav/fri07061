01b37ea0b1980e3bca96b85472466aa5c9c0478d
subayadhav <suba10499@gmail.com>
2019-06-07 09:09:42 +0530

    Create 123.py

---
+++ 123.py
@@ -0,0 +1,13 @@
+n1=int(input())
+l=[]
+a=0
+for i in range(2,n1//2):
+	if n%i==0:
+		for j in range(2,i//2+1):
+			if i%j==0:
+				a=1
+				break
+		if a!=1:
+			l.append(i)
+		a=0
+print(*l,sep=' ')
